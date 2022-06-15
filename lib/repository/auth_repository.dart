604ed2c863fcc66f07fa1b77cc6083b9c3d56c63
Kaaji <au.anupupadhaya@gmail.com>
2022-06-15 14:07:18 +0545

    Auth repository setup

---
+++ lib/repository/auth_repository.dart
@@ -0,0 +1,36 @@
+
+import 'package:flutter_application/data/network/base_api_services.dart';
+import 'package:flutter_application/data/network/network_api_services.dart';
+
+import '../resources/components/app_url.dart';
+
+class AuthRepository {
+
+  BaseApiServices _apiServices = NetworkApiService(); 
+
+  Future<dynamic> loginApi (dynamic data)async {
+    
+    try{
+
+      dynamic response = await _apiServices.getPostApiResponse(AppUrl.loginEndPoint, data);
+      return response; 
+
+    }catch(e){
+      throw e;
+    }
+  }
+
+
+  Future<dynamic> registerApi (dynamic data)async {
+    
+    try{
+
+      dynamic response = await _apiServices.getPostApiResponse(AppUrl.registerApiEndPoint, data);
+      return response; 
+
+    }catch(e){
+      throw e;
+    }
+  }
+
+}
